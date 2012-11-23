c2e03f5dc5b79300d792089f1f49bb4bc509c27c
Alexandru-Sever Horin <alex.sever.h@gmail.com>
2012-11-23 07:26:41 +0200

    Remade bluetooth interpretor

--- robot/command_interpretor.c
+++ robot/command_interpretor.c
@@ -20,44 +20,72 @@ cmd_motor_lr_t cmd_motor_lr;
 
 int parameters[5];
 
+u32 command_queue;
+int cmd_index;
+
+
+
+int check_command(char cmd_byte, const char *cmd_str, u32 cmd_queue_flag, int param_nr, int cmd_index)
+{
+	// if other command --> quit
+	if((command_queue != 0) && (command_queue & cmd_queue_flag == 0))
+		return 0;
+
+	if (cmd_index >= 0 && cmd_index < (int)strlen(cmd_str))
+	{
+		if(cmd_str[cmd_index] == cmd_byte)
+			command_queue |= cmd_queue_flag;
+		else
+			command_queue &= ~cmd_queue_flag;
+	}
+	else if (cmd_index >= strlen(cmd_str) && cmd_index < strlen(cmd_str) + param_nr)
+	{
+		parameters[cmd_index - strlen(cmd_str)] = cmd_byte;
+	}
+	else if (cmd_index == strlen(cmd_str) + param_nr)
+	{
+		if (cmd_str[cmd_index] == '\r')
+		{
+			command_queue = 0;
+			return 1;
+		}
+		else
+		{
+			command_queue &= ~cmd_queue_flag;
+			return 0;
+		}
+	}
+	return 0;
+}
+
+
+
 void interpret_byte(char cmd)
 {
-	static int cmd_index;
-	static u32 command_queue;
+	int res;
 
-	// if expecting a motor command
-	if (command_queue & MOTOR_LR_QUEUE)
-	{
-
-		if (cmd_index < strlen(MOTOR_LR_CMD))     // if still processing the command
-		{
-			if (cmd != MOTOR_LR_CMD[cmd_index])
-				command_queue &= ~MOTOR_LR_QUEUE;
-		}
-
-		if (cmd_index >= strlen(MOTOR_LR_CMD) && cmd_index < strlen(MOTOR_LR_CMD) + 2)    // if reading parameters
-		{
-			parameters[cmd_index - strlen(MOTOR_LR_CMD)] = cmd;
-		}
-		else      // expect END_CMD
-		{
-			if(cmd == END_CMD) // accept command
-			{
-				motor_control_lr(parameters[0], parameters[1]);
-				usart_puts(MOTOR_LR_CMD);
-				command_queue &= ~MOTOR_LR_QUEUE;
-			}
-		}
-
-	}
-
-	if (!(command_queue & (MOTOR_LR_QUEUE)))
-	{
-		command_queue = ~0;
+	if(command_queue == 0)
 		cmd_index = 0;
-	}
-	else
-	{
-		cmd_index++;
-	}
+
+	res = check_command(cmd, MOTOR_LR_CMD, MOTOR_LR_QUEUE, 2, cmd_index);
+	if(res)
+		motor_control_lr(parameters[0], parameters[1]);
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
